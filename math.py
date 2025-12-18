5ff24215eabc4d0c9c330e57b813f2f06f634c93
Sai-Govardhan <24331a05i6@mvgrce.edu.in>
2025-12-18 23:41:25 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,11 @@
+#24331A05I6
+
+import math
+print("Value of pi : ",math.pi)
+n=81
+print("Square root of ",n,"is",math.sqrt(n))
+angle = 30
+angle_rad = math.radians(angle)
+print("cos", angle, "is", math.cos(angle_rad))
+print("sin", angle, "is", math.sin(angle_rad))
+print("tan",angle,"is",math.tan(angle_rad))
